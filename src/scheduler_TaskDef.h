e84b0f36ad5abe28ba695d2b9d9fc45c9387dc7c
Daniel Grünewald <daniel.gruenewald@itwm.fhg.de>
2017-10-04 14:33:23 +0200

    Some adjustments in layout of TaskDef implementation

--- src/scheduler_TaskDef.h
+++ src/scheduler_TaskDef.h
@@ -17,133 +17,131 @@
 namespace scheduler {
 
 template <typename State>
-class TaskDef
-{
+class TaskDef {
 
-	public:
+public:
 
-		TaskDef
-		  ()
-		: pre_conditions_ ()
-		, executables_    ()
-		, post_conditions_()
-		{}
+    TaskDef
+      ()
+    : pre_conditions_ ()
+    , executables_    ()
+    , post_conditions_()
+    {}
 
-		~TaskDef() {};
+    ~TaskDef() {};
 
-		// insert a pre-condition
-		void
-		insert
-		  ( std::unique_ptr<PreCondition<State> > preCondition )
-		{
-			if(static_cast<bool>(preCondition)) {
+    // insert a pre-condition
+    void
+    insert
+      ( std::unique_ptr<PreCondition<State> > preCondition ) {
 
-				if(!static_cast<bool>(pre_conditions_)) {
-					pre_conditions_.reset( new PreConditionList<State> );
-				}
+      if(static_cast<bool>(preCondition)) {
 
-				pre_conditions_->insert( std::move(preCondition) );
-			}
-		}
-
-		bool
-		hasPreCondition
-		  () const {
-		  return static_cast<bool>(pre_conditions_);
-		}
-
-		inline PreCondition<State> const &
-        getPreCondition
-          () const {
-          return *pre_conditions_;
+        if(!static_cast<bool>(pre_conditions_)) {
+            pre_conditions_.reset( new PreConditionList<State> );
         }
 
-        inline PreCondition<State> &
-        getPreCondition
-          () {
-          return *pre_conditions_;
+        pre_conditions_->insert( std::move(preCondition) );
+      }
+    }
+
+    bool
+    hasPreCondition
+      () const {
+      return static_cast<bool>(pre_conditions_);
+    }
+
+    inline PreCondition<State> const &
+    getPreCondition
+      () const {
+      return *pre_conditions_;
+    }
+
+    inline PreCondition<State> &
+    getPreCondition
+      () {
+      return *pre_conditions_;
+    }
+
+    // insert an executable
+    void
+    insert
+      ( std::unique_ptr<Executable<State> > executable ) {
+
+      if(static_cast<bool>(executable)) {
+
+        if( !static_cast<bool>(executables_) ) {
+            executables_.reset(new ExecutableList<State>);
         }
 
-		// insert an executable
-		void
-		insert
-		  ( std::unique_ptr<Executable<State> > executable )
-		{
-			if(static_cast<bool>(executable)) {
+        executables_->insert( std::move(executable) );
+      }
+    }
 
-				if( !static_cast<bool>(executables_) ) {
-					executables_.reset(new ExecutableList<State>);
-				}
+    bool
+    hasExecutable
+      () const {
+      return static_cast<bool>(executables_);
+    }
 
-				executables_->insert( std::move(executable) );
-			}
-		}
+    inline Executable<State> const &
+    getExecutable
+      () const {
+      return *executables_;
+    }
 
-		bool
-        hasExecutable
-          () const {
-          return static_cast<bool>(executables_);
+    inline Executable<State> &
+    getExecutable
+      () {
+      return *executables_;
+    }
+
+    // insert a post-condition
+    void
+    insert
+      ( std::unique_ptr<PostCondition<State> > postCondition  ) {
+
+      if(static_cast<bool>(postCondition)) {
+
+        if( !static_cast<bool>(post_conditions_) ) {
+            post_conditions_.reset(new PostConditionList<State>);
         }
 
-        inline Executable<State> const &
-        getExecutable
-          () const {
-          return *executables_;
-        }
+        post_conditions_->insert( std::move(postCondition) );
+      }
+    }
 
-        inline Executable<State> &
-        getExecutable
-          () {
-          return *executables_;
-        }
+    bool
+    hasPostCondition
+      () const {
+      return static_cast<bool>(post_conditions_);
+    }
 
-		// insert a post-condition
-		void
-		insert
-		  ( std::unique_ptr<PostCondition<State> > postCondition  ) {
+    inline PostCondition<State> const &
+    getPostCondition
+      () const {
+      return *post_conditions_;
+    }
 
-			if(static_cast<bool>(postCondition)) {
+    inline PostCondition<State> &
+    getPostCondition
+      () {
+      return *post_conditions_;
+    }
 
-				if( !static_cast<bool>(post_conditions_) ) {
-					post_conditions_.reset(new PostConditionList<State>);
-				}
+    bool
+    empty() {
 
-				post_conditions_->insert( std::move(postCondition) );
+      return ( not(hasPreCondition()) &&
+               not(hasExecutable())   &&
+               not(hasPostCondition()) );
+    }
 
-			}
-		}
+public:
 
-		bool
-        hasPostCondition
-          () const {
-          return static_cast<bool>(post_conditions_);
-        }
-
-        inline PostCondition<State> const &
-        getPostCondition
-          () const {
-          return *post_conditions_;
-        }
-
-        inline PostCondition<State> &
-        getPostCondition
-          () {
-          return *post_conditions_;
-        }
-
-		bool
-		empty()
-		{
-		  return ( not(hasPreCondition()) &&
-		           not(hasExecutable())   &&
-		           not(hasPostCondition()) );
-		}
-
-	public:
-
-		std::unique_ptr<PreConditionList<State>>  pre_conditions_;
-		std::unique_ptr<ExecutableList<State>>    executables_;
-		std::unique_ptr<PostConditionList<State>> post_conditions_;
+    std::unique_ptr<PreConditionList<State>>  pre_conditions_;
+    std::unique_ptr<ExecutableList<State>>    executables_;
+    std::unique_ptr<PostConditionList<State>> post_conditions_;
 
 };
 
